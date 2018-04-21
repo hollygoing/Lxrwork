dd28149370c0e1f5eeed67760eddb84a0bd5fc63
hollygoing <1037074516@qq.com>
2018-04-21 21:28:18 +0800

    计时器和倒计时功能

---
+++ app/src/main/java/com/example/leixiaorong_de_bigwork_finally/activity/activity/StopwatchActivity.java
@@ -0,0 +1,78 @@
+package com.example.leixiaorong_de_bigwork_finally.activity.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.Toast;
+
+import com.example.leixiaorong_de_bigwork_finally.R;
+
+public class StopwatchActivity extends AppCompatActivity implements View.OnClickListener,Chronometer.OnChronometerTickListener{
+
+    private Chronometer chronometer;
+    private Button btn_start,btn_stop,btn_base;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        initView();
+    }
+
+    private void initView() {
+        chronometer = (Chronometer) findViewById(R.id.chronometer);
+        btn_start = (Button) findViewById(R.id.btnStart);
+        btn_stop = (Button) findViewById(R.id.btnStop);
+        btn_base = (Button) findViewById(R.id.btnReset);
+
+        chronometer.setOnChronometerTickListener(this);
+        btn_start.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
+        btn_base.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnStart:
+                chronometer.start();// 开始计时
+                break;
+            case R.id.btnStop:
+                chronometer.stop();// 停止计时
+                break;
+            case R.id.btnReset:
+                chronometer.setBase(SystemClock.elapsedRealtime());// 复位
+                break;
+        }
+    }
+
+    @Override
+    public void onChronometerTick(Chronometer chronometer) {
+        String time = chronometer.getText().toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.stopwatch_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_back:
+                Intent intent = new Intent(StopwatchActivity.this,MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
